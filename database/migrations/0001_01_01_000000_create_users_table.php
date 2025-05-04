ff3e5570f046019ea5a2bc700a24ee2e09ed542f
AliHossain09 <alihoossainn@gmail.com>
2025-05-04 12:12:56 +0600

    table create migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('userName');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 
-Route::get('/', function () { return view('auth.login'); })->name('home');
-Route::get('/registration', function () { return view('auth.registration'); });
+Route::get('/', function () { return view( 'auth.login' ); })->name('home');
+
+Route::get('/registration', function () { return view( 'auth.registration' ); })->name('registration.show');
+Route::post('/registrationStore', [ AuthController::class, 'registrationStore' ] )->name('registration.store');
+
